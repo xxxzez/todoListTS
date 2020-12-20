79985acacc82885719a2f2c738773f32c222a730
Matvei Bohush <bohush.mathew@gmail.com>
2020-12-20 19:51:35 +0300

    add redux-devtools

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,13 @@
 import { tasksReducer } from './tasks-reducer'
 import { todoListsReducer } from './todolists-reducer'
 import { combineReducers, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todoListsReducer,
 })
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // @ts-ignore
