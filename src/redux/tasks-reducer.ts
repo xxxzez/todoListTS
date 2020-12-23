81307821043241d3acb82500922cc627bc264f7f
Matvei Bohush <bohush.mathew@gmail.com>
2020-12-24 01:25:16 +0300

    ref

--- src/redux/tasks-reducer.ts
+++ src/redux/tasks-reducer.ts
@@ -75,22 +75,18 @@ export const tasksReducer = (
             return stateCopy
         }
         case 'CHANGE-TASK-STATUS': {
-            let todoListTasks = state[action.todolistId]
-            const task = todoListTasks.find((t) => t.id === action.taskId)
-            if (task) {
-                task.isDone = action.isDone
-            }
-            state[action.todolistId] = [...todoListTasks]
+            let todolistTasks = state[action.todolistId]
+            state[action.todolistId] = todolistTasks.map((t) =>
+                t.id === action.taskId ? { ...t, isDone: action.isDone } : t
+            )
             return { ...state }
         }
         case 'CHANGE-TASK-TITLE': {
-            const stateCopy = { ...state }
-            const tasks = stateCopy[action.todolistId]
-            const task = tasks.find((t) => t.id === action.taskId)
-            if (task) {
-                task.title = action.title
-            }
-            return stateCopy
+            let todolistTasks = state[action.todolistId]
+            state[action.todolistId] = todolistTasks.map((t) =>
+                t.id === action.taskId ? { ...t, title: action.title } : t
+            )
+            return { ...state }
         }
         case 'ADD-TODOLIST': {
             const stateCopy = { ...state }
