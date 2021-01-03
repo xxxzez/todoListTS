4a175cefdba4ac423c11bc0864d2d16b9b875a25
Matvei Bohush <bohush.mathew@gmail.com>
2021-01-04 00:42:28 +0300

    add jest-environment-puppeteer

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -2,7 +2,7 @@ describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
         await page.goto(
-            'http://localhost:9009/iframe.html?id=additemform-component--add-item-form-base-example'
+            'http://localhost:6006/iframe.html?id=additemform-component--add-item-form-base-example&viewMode=story'
         )
         const image = await page.screenshot()
 
--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: 'jest-puppeteer',
+    testEnvironment: 'jest-environment-puppeteer',
     testRegex: './*\\.test\\.js$',
     setupFilesAfterEnv: ['./setupTests.js'],
 }
