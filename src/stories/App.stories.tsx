764d3714687fb51e3940c8219792b3b285fa9acc
Matvei Bohush <bohush.mathew@gmail.com>
2021-01-17 18:37:30 +0300

    add decorator

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -2,19 +2,12 @@ import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { App } from '../App'
+import { ReduxStoreProviderDecorator } from './decorators/ReduxStoreProviderDecorator'
 
 export default {
     title: 'Todolist/App',
     component: App,
-    argTypes: {
-        onChange: {
-            description: 'Changed value editable span',
-        },
-        title: {
-            defaultValue: 'HTML',
-            description: 'Start value to editable span',
-        },
-    },
+    decorators: [ReduxStoreProviderDecorator]
 } as Meta
 
 const Template: Story = () => <App />
---
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from '../../redux/store'
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => (
+    <Provider store={store}>{storyFn()}</Provider>
+)
