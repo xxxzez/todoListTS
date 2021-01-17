edd33024dd42aba3f7c3123fe66234a9110e23d0
Matvei Bohush <bohush.mathew@gmail.com>
2021-01-17 17:59:36 +0300

    delete unused imports

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { action } from '@storybook/addon-actions'
 import { Task, TaskPropsType } from '../Task'
-import { v1 } from 'uuid'
 
 export default {
     title: 'Todolist/Task',
